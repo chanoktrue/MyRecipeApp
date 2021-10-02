c22d39f1b3e7fd56c1d53b4acc3082a9cdc61e87
Thongchai Subsaidee <chanoktrue@me.com>
2021-10-02 21:49:32 +0700

    strat tutorial

---
+++ MyRecipeApp/MyRecipeApp-iOS/MyRecipeApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipeApp_iOSApp.swift
+//  MyRecipeApp-iOS
+//
+//  Created by Thongchai Subsaidee on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipeApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyRecipeApp/MyRecipeApp-macOS/MyRecipeApp_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipeApp_macOSApp.swift
+//  MyRecipeApp-macOS
+//
+//  Created by Thongchai Subsaidee on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipeApp_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
